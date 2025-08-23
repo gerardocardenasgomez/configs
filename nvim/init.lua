dca8890208b1e0d97194e1b2f56c6db2205170fd
Gerardo Cardenas-Gomez <gerardo@gerardobsd.com>
2025-08-23 01:04:01 -0500

    harpoon

--- nvim/init.lua
+++ nvim/init.lua
@@ -358,6 +358,12 @@ require('lazy').setup({
   --
   -- Use the `dependencies` key to specify the dependencies of a particular plugin
 
+  {
+    'ThePrimeagen/harpoon',
+    branch = 'harpoon2',
+    dependencies = { 'nvim-lua/plenary.nvim', 'nvim-telescope/telescope.nvim' },
+  },
+
   { -- Fuzzy Finder (files, lsp, etc)
     'nvim-telescope/telescope.nvim',
     event = 'VimEnter',
@@ -1033,6 +1039,64 @@ require('lazy').setup({
     },
   },
 })
+
+local harpoon = require 'harpoon'
+
+-- REQUIRED
+harpoon:setup()
+-- REQUIRED
+
+-- local conf = require('telescope.config').values
+-- local function toggle_telescope(harpoon_files)
+--   local file_paths = {}
+--   for _, item in ipairs(harpoon_files.items) do
+--     table.insert(file_paths, item.value)
+--   end
+--
+--   require('telescope.pickers')
+--     .new({}, {
+--       prompt_title = 'Harpoon',
+--       finder = require('telescope.finders').new_table {
+--         results = file_paths,
+--       },
+--       previewer = conf.file_previewer {},
+--       sorter = conf.generic_sorter {},
+--     })
+--     :find()
+-- end
+
+-- vim.keymap.set('n', '<C-e>', function()
+--   toggle_telescope(harpoon:list())
+-- end, { desc = 'Open harpoon window' })
+
+vim.keymap.set('n', '<leader>w', function()
+  print 'test'
+  harpoon:list():add()
+end)
+vim.keymap.set('n', '<C-w>', function()
+  harpoon.ui:toggle_quick_menu(harpoon:list())
+end)
+
+vim.keymap.set('n', '<C-h>', function()
+  harpoon:list():select(1)
+end)
+vim.keymap.set('n', '<C-j>', function()
+  harpoon:list():select(2)
+end)
+vim.keymap.set('n', '<C-k>', function()
+  harpoon:list():select(3)
+end)
+vim.keymap.set('n', '<C-l>', function()
+  harpoon:list():select(4)
+end)
+
+-- Toggle previous & next buffers stored within Harpoon list
+vim.keymap.set('n', '<C-q>', function()
+  harpoon:list():prev()
+end)
+vim.keymap.set('n', '<C-e>', function()
+  harpoon:list():next()
+end)
 
 local capabilities = require('blink.cmp').get_lsp_capabilities()
 
